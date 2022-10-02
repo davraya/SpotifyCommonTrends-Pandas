054a0c99cfb391f33b9127f556a7f091dc11e80e
David Raya <85646734+davraya@users.noreply.github.com>
2022-10-02 00:24:17 -0600

    Added comments

--- main.py
+++ main.py
@@ -1,22 +1,41 @@
 import pandas as pd
 
 
-# popularity is based on how much a song has been played recently. 
 
-df = pd.read_csv("unpopular_songs.csv") # creates a DataFrame()
+# This creates a pandas.DataFrame
+df = pd.read_csv("unpopular_songs.csv")
 
+# the numbe of rows in the DataFrame
 number_of_songs = df.index.stop
 
+"""
+Get elements in Range:
+    The purpose of this function is to take a pandas.Series
+    and basically take and create subseries based on a range
+    of values and return the size of such subserie. 
+"""
 def get_elements_in_range(column_name, start, multiplicator=0.1, end=1):
+    # Reducing the subserie to just the lower part of the start
     lower_limit_elements = df[df[column_name] < multiplicator * start]
+    # Reducing the subserie to just the uppe limit
     upper_limit_elements = df[df[column_name] < multiplicator * (start + end)]
 
+    # Example: If we have a Serie with possible numbers (Each value will most likley be 
+    # unique with decimal numbers) with between 1 and 10 and we want to know how
+    # many elements have a value between 3 and 4 we will first count all the values 
+    # from 1 to 3 (lower limmit count) then we will count all the values form 1 to 4 (upper limit count).
+    # Then we simply subtract lower limit count from the upper limit count. 
+      
+    
     elements_in_range = len(upper_limit_elements) - len(lower_limit_elements)
 
     return(f"The number of songs with a {column_name} between {start} and {start + 1} is {elements_in_range}")
 
+
+# Here I use list comprehension 
 danceability = [get_elements_in_range("danceability", start) for start in range(10)]
-print(*danceability, sep='\n')
+# The * is called the unpack operator
+print(*danceability, sep='\n') 
 print()
 danceability = [get_elements_in_range("energy", start) for start in range(10)]
 print(*danceability, sep='\n')
